f897eb1cf75554fe4783c0d9bf86feaf9197495e
funyug <shivamchawla243@gmail.com>
2018-02-09 13:49:59 +0530

    Refactored for recycler views

--- app/src/main/java/net/blockchaind/coderswag/Adapter/CategoryRecycleAdapter.kt
+++ app/src/main/java/net/blockchaind/coderswag/Adapter/CategoryRecycleAdapter.kt
@@ -1,5 +1,44 @@
 package net.blockchaind.coderswag.Adapter
 
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import kotlinx.android.synthetic.main.category_list_item.view.*
+import net.blockchaind.coderswag.Model.Category
+import net.blockchaind.coderswag.R
+
 /**
  * Created by shiva on 2/9/2018.
  */
+
+class CategoryRecycleAdapter(val context:Context, val categories:List<Category>) : RecyclerView.Adapter<CategoryRecycleAdapter.Holder>() {
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): Holder {
+        val view = LayoutInflater.from(context)
+                .inflate(R.layout.category_list_item,parent,false)
+        return Holder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return categories.count()
+    }
+
+    override fun onBindViewHolder(holder: Holder?, position: Int) {
+        holder?.bindCategory(categories[position],context)
+    }
+
+    inner class Holder(itemView: View?) : RecyclerView.ViewHolder(itemView) {
+        val categoryImage = itemView?.findViewById<ImageView>(R.id.categoryImage)
+        val categoryText = itemView?.findViewById<TextView>(R.id.categoryText)
+
+        fun bindCategory(category: Category, context: Context) {
+            val resourceId = context.resources.getIdentifier(category.image,
+                    "drawable",context.packageName)
+            categoryImage?.setImageResource(resourceId)
+            categoryText?.text = category.title
+        }
+    }
+}
